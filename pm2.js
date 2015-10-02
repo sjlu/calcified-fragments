bd1da3f7aee3f696472d903c045192bf7b1bcc28
Steven Lu <tacticalazn@gmail.com>
2015-10-01 20:56:32 -0400

    preload card data before boot

--- pm2.js
+++ pm2.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var pm2 = require('pm2');
 
-var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
-var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
+var instances = process.env.WEB_CONCURRENCY || -1;
+var maxMemory = process.env.WEB_MEMORY || 512;
 
 pm2.connect(function() {
   pm2.start({
