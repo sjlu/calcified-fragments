04249f3183d11e9939b98517f9a5cb889d9c68c8
Steven Lu <tacticalazn@gmail.com>
2016-07-09 01:55:58 -0400

    bug fixes and cleanups

--- routes/index.js
+++ routes/index.js
@@ -21,10 +21,6 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/faq', function(req, res) {
-  res.render('faq');
-})
-
 router.post('/', function(req, res) {
 
   var platform = 'xbox'
