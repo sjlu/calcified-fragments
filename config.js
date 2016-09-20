d2a33631b108d50b9a2af947c7c79fe227b7ac63
Steven Lu <tacticalazn@gmail.com>
2016-09-20 09:06:51 -0400

    adding in new ghosts and videos

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-var _ = require('lodash');
-var dotenv = require('dotenv');
+var _ = require('lodash')
+var dotenv = require('dotenv')
 
 // run in silent mode
-dotenv.config({silent:true})
+dotenv.config({
+  silent:true
+})
 
 // load dotenv config vars if available
-dotenv.load();
+dotenv.load()
 
 var config = {
   // https://www.bungie.net/en/User/API,
   ENV: 'production',
-  BUNGIE_API_KEY: ""
-};
-config = _.defaults(process.env, config);
+  BUNGIE_API_KEY: '',
+  LOG_LEVEL: 'info'
+}
 
-module.exports = config;
+config = _.defaults(process.env, config)
+
+module.exports = config
